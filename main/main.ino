7faa67c111e137af5c33e79c155afa431c4976ef
trangnguyen3010 <66387098+trangnguyen3010@users.noreply.github.com>
2024-07-06 15:03:49 -0600

    add a button check for the 4 buttons added to the circuit

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,18 @@
+#include <LiquidCrystal.h>  //the liquid crystal library contains commands for printing to the display
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);  // tell the RedBoard what pins are connected to the display
+
+struct Button {
+  String name;
+  int pin;
+};
+
+Button yellowButton = {"Yellow", A0};
+Button greenButton = {"Green", A1};
+Button redButton = {"Red", A2};
+Button blueButton = {"Blue", A3};
+Button buttons[] = { yellowButton, greenButton, redButton, blueButton };
+
 //the right motor will be controlled by the motor A pins on the motor driver
 const int AIN1 = 13;  //control pin 1 on the motor driver for the right motor
 const int AIN2 = 12;  //control pin 2 on the motor driver for the right motor
@@ -8,16 +23,23 @@ const int PWMB = 10;  //speed control pin on the motor driver for the left motor
 const int BIN2 = 9;   //control pin 2 on the motor driver for the left motor
 const int BIN1 = 8;   //control pin 1 on the motor driver for the left motor
 
-int switchPin = 7;  //switch to turn the robot on and off
-
 const int driveTime = 20;  //this is the number of milliseconds that it takes the robot to drive 1 inch
                            //it is set so that if you tell the robot to drive forward 25 units, the robot drives about 25 inches
 
 String distance;  //the distance to travel in each direction
 
+String names[] = { "trang", "minh", "anh" };
+int currentPerson = 0;
+
 /********************************************************************************/
 void setup() {
-  pinMode(switchPin, INPUT_PULLUP);  //set this as a pullup to sense whether the switch is flipped
+  //set all of the button pins to input_pullup (use the built-in pull-up resistors)
+  for (int i = 0; i < 4; i++) {
+    pinMode(buttons[i].pin, INPUT_PULLUP);
+  }
+
+  lcd.begin(16, 2);  //tell the lcd library that we are using a display that is 16 characters wide and 2 characters high
+  lcd.clear();
 
   //set the motor control pins as outputs
   pinMode(AIN1, OUTPUT);
@@ -29,31 +51,31 @@ void setup() {
   pinMode(PWMB, OUTPUT);
 
   Serial.begin(9600);  //begin serial communication with the computer
-
-  //prompt the user to enter a command
-  Serial.println("Enter a direction followed by a distance.");
-  Serial.println("f = forward, b = backward, r = turn right, l = turn left");
-  Serial.println("Example command: f 50");
 }
 
 /********************************************************************************/
 void loop() {
-  if (digitalRead(7) == LOW) {   //if the switch is in the ON position
-    if (Serial.available() > 0)  //if the user has sent a command to the RedBoard
-    {
-      distance = Serial.readStringUntil(' ');  //read the characters in the command until you reach the second space
+  buttonCheck();
+  displayOnLcd();
+  if (Serial.available() > 0)  //if the user has sent a command to the RedBoard
+  {
+    distance = Serial.readStringUntil(' ');  //read the characters in the command until you reach the second space
 
-      //print the command that was just received in the serial monitor
-      Serial.print(" ");
-      Serial.println(distance.toInt());
+    //print the command that was just received in the serial monitor
+    Serial.print(" ");
+    Serial.println(distance.toInt());
 
-      rightMotor(-200);                     //drive the right wheel forward
-      leftMotor(-200);                      //drive the left wheel forward
-      delay(driveTime * distance.toInt());  //drive the motors long enough travel the entered distance
-      rightMotor(0);                        //turn the right motor off
-      leftMotor(0);                         //turn the left motor off
-    }
+    rightMotor(-200);                     //drive the right wheel forward
+    leftMotor(-200);                      //drive the left wheel forward
+    delay(driveTime * distance.toInt());  //drive the motors long enough travel the entered distance
+    rightMotor(0);                        //turn the right motor off
+    leftMotor(0);                         //turn the left motor off
   }
+}
+
+void displayOnLcd() {
+  lcd.setCursor(0, 0);                             //set the cursor to the 0,0 position (top left corner)
+  lcd.print("Next turn:" + names[currentPerson]);  //print hello, world! starting at that position
 }
 
 /********************************************************************************/
@@ -92,4 +114,16 @@ void leftMotor(int motorSpeed)  //function for driving the left motor
     digitalWrite(BIN2, LOW);  //set pin 2 to low
   }
   analogWrite(PWMB, abs(motorSpeed));  //now that the motor direction is set, drive it at the entered speed
+}
+
+/********************************************************************************/
+//CHECK WHICH BUTTON IS PRESSED
+void buttonCheck() {
+  //check if any buttons are being pressed
+  //set all of the button pins to input_pullup (use the built-in pull-up resistors)
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(buttons[i].pin) == LOW) {
+      Serial.println("button " + String(buttons[i].name) + " is pressed!");
+    }
+  }
 }
